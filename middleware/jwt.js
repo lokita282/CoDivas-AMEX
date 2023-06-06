18b5b3b3ef9878ea218a95b44e85bb4595a38d82
vidhitapai <vidhitapai@gmail.com>
2023-06-06 23:32:15 +0530

    added middleware auth for specific user roles

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -1,37 +1,101 @@
 // Importing modules
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv").config();
-const User = require("../models/user");
-const { removeSensitiveData } = require("../utils/functions");
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv').config();
+const User = require('../models/user');
+const { removeSensitiveData } = require('../utils/functions');
 
 // JWT Authorization
 const authorizeJWT = {
-  verifyJWT: async (req, res, next) => {
-    try {
-      let token = req.header("Authorization").replace("Bearer ", "");
-      let decoded = jwt.verify(token, process.env.JWT_SECRET);
-      let user = await User.findOne({
-        _id: decoded._id,
-        "tokens.token": token,
-      });
+    verifyJWT: async (req, res, next) => {
+        try {
+            let token = req.header('Authorization').replace('Bearer ', '');
+            let decoded = jwt.verify(token, process.env.JWT_SECRET);
+            let user = await User.findOne({
+                _id: decoded._id,
+                'tokens.token': token
+            });
 
-      if (!user) {
-        res.status(401).json({
-          message: "Please Authenticate!",
-        });
-        return;
-      }
+            if (!user) {
+                res.status(401).json({
+                    message: 'Please Authenticate!'
+                });
+                return;
+            }
 
-      req.user = user;
-      req.token = token;
+            req.user = user;
+            req.token = token;
 
-      next();
-    } catch (error) {
-      res.status(400).json({
-        message: error.message,
-      });
+            next();
+        } catch (error) {
+            res.status(400).json({
+                message: error.message
+            });
+        }
+    },
+
+    roleMerchant: (req, res, next) => {
+        try {
+            if (req.user.type === 'merchant') {
+                next();
+            } else {
+                res.status(403).json({
+                    message: 'Access Denied'
+                });
+            }
+        } catch (error) {
+            res.status(500).json({
+                message: error.message
+            });
+        }
+    },
+
+    roleBank: (req, res, next) => {
+        try {
+            if (req.user.type === 'bank') {
+                next();
+            } else {
+                res.status(403).json({
+                    message: 'Access Denied'
+                });
+            }
+        } catch (error) {
+            res.status(500).json({
+                message: error.message
+            });
+        }
+    },
+
+    roleBeneficiary: (req, res, next) => {
+        try {
+            if (req.user.type === 'beneficiary') {
+                next();
+            } else {
+                res.status(403).json({
+                    message: 'Access Denied'
+                });
+            }
+        } catch (error) {
+            res.status(500).json({
+                message: error.message
+            });
+        }
+    },
+
+    roleOrganisation: (req, res, next) => {
+        try {
+            if (req.user.type === 'organisation') {
+                next();
+            } else {
+                res.status(403).json({
+                    message: 'Access Denied'
+                });
+            }
+        } catch (error) {
+            res.status(500).json({
+                message: error.message
+            });
+        }
     }
-  },
 };
 
 // Exporting Module
