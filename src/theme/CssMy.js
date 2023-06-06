3fa53ba1c4ba5185eb0060786e774bac1a0c5eaa
Khushi Mehta <khushimehta8282@gmail.com>
2023-06-06 19:57:41 +0530

    theme : theme setup

---
+++ src/theme/CssMy.js
@@ -0,0 +1,17 @@
+export const btn = { textTransform: 'none', fontSize: '14px', fontFamily: 'Poppins', color: '#3770FF', border: '2px solid #3770FF' }
+export const card = { backgroundColor: 'white', padding: '10px', borderRadius: '10px', height: '100%', margingBottom: '0%', paddingBottom: '0% !important' }
+export const text = { fontFamily: 'Poppins' }
+export const ptag = { fontSize: '13px', fontFamily: 'Poppins', color: '#6A707F', margin: '0', padding: '0' }
+export const df_jc_ac = { display: 'flex', justifyContent: 'center', alignItems: 'center' }
+export const df_jfe_ac = { display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }
+export const df_jfs_ac = { display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }
+export const df_jfs_ac_fdc = { display: 'flex', justifyContent: 'center', alignItems: 'flex-start', flexDirection: 'column' }
+export const df_jc_ac_fdc = { display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }
+export const textField = { width: '100%', marginBottom: '2%', backgroundColor: 'transparent', boxShadow: '0px 1px 26px rgba(94, 99, 116, 0.05)' }
+export const circularImage = { borderRadius: '50%', width: '100px', height: '100px', objectFit: 'cover' }
+export const bold_name = { fontFamily: 'Poppins', fontWeight: 'bold' }
+export const btn_connect = { textTransform: 'none', fontSize: '14px', borderRadius: '50px', fontFamily: 'Poppins', color: '#375EC0', border: '2px solid #375EC0', width: '100%', '&:hover': { backgroundColor: '#375EC0', color: 'white' } }
+export const btn_hire = { textTransform: 'none', fontSize: '14px', borderRadius: '50px', fontFamily: 'Poppins', color: 'white', border: '2px solid #009946', backgroundColor: '#009946', width: '100%', '&:hover': { backgroundColor: 'transparent', color: '#009946' } }
+export const circularprog = { backgroundColor: '#3770FF', color: 'white', borderRadius: '50%', padding: '5px' }
+export const link = { color: '#375EC0', textDecoration: 'underline', cursor: 'pointer' }
+
