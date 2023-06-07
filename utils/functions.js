28ac96f3e6a0ee5dacb5ed1153b732d296d32ca8
vidhitapai <vidhitapai@gmail.com>
2023-06-08 00:03:15 +0530

    added generateNumber utility

--- utils/functions.js
+++ utils/functions.js
@@ -6,8 +6,22 @@ const removeSensitiveData = (data) => {
     data.__v = undefined;
     data.profilepicture = undefined;
     data.otp = undefined;
-    
-    return data;
-  };
 
-module.exports = {removeSensitiveData}
+    return data;
+};
+
+module.exports = { removeSensitiveData };
+
+const generateRandomNumber = (numLength) => {
+    let digits = '0123456789';
+    let num = '';
+    for (let i = 0; i < numLength; i++) {
+        num += digits[Math.floor(Math.random() * 10)];
+    }
+    return num;
+};
+
+module.exports = {
+    removeSensitiveData,
+    generateRandomNumber
+};
