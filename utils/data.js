bf1f875ffa62a1247e656a7a2ce7540a0e2d891d
vidhitapai <vidhitapai@gmail.com>
2023-06-08 18:09:45 +0530

    utility data

---
+++ utils/data.js
@@ -0,0 +1,50 @@
+const shortCodes = {
+    health: 'HEA',
+    agriculture: 'AGR',
+    education: 'EDU',
+    food: 'FOD',
+    housing: 'HOU',
+    transportation: 'TRA',
+    utility: 'UTI',
+    telecommunication: 'TEL',
+    other: 'OTH'
+};
+
+const organisationDetails = [
+    {
+        orgId: 678431,
+        orgName: 'Think360',
+        orgLogo:
+            'https://res.cloudinary.com/dlwpoojua/image/upload/v1686227018/amex-hackathon/think360_eb4917.png'
+    },
+    {
+        orgId: 689346,
+        orgName: 'Stark Industries',
+        orgLogo:
+            'https://res.cloudinary.com/dlwpoojua/image/upload/v1686227186/amex-hackathon/stark_qhw2ee.png'
+    },
+    {
+        orgId: 549012,
+        orgName: 'National Health Authority of India',
+        orgLogo:
+            'https://res.cloudinary.com/dlwpoojua/image/upload/v1686227187/amex-hackathon/nha_mbgupt.png'
+    },
+    {
+        orgId: 549012,
+        orgName: 'Odisha Government',
+        orgLogo:
+            'https://res.cloudinary.com/dlwpoojua/image/upload/v1686227382/amex-hackathon/odishaGovt_xcs0ek.png'
+    },
+    {
+        orgId: 120871,
+        orgName: 'Pratham Foundation',
+        orgLogo:
+            'https://res.cloudinary.com/dlwpoojua/image/upload/v1686227613/amex-hackathon/pratham_tvjgwi.png'
+    },
+    {
+        orgId: 989791,
+        orgName: 'Goonj Foundation',
+        orgLogo:
+            'https://res.cloudinary.com/dlwpoojua/image/upload/v1686227613/amex-hackathon/goonj_tu8yre.png'
+    }
+];
