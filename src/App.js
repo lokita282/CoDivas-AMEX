badd093b7c743b66fe6dbf81709d4a27255ef8e8
Khushi Mehta <khushimehta8282@gmail.com>
2023-06-10 20:38:29 +0530

    setup : router and toasts

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import { merchant } from './context/MainContext';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import MainRouter from './router/MainRouter';
+import { useState } from 'react';
+
+function App() {
+  const [user, setUser] = useState(null)
+  const [token, setToken] = useState(null)
+  const context = {
+    user, setUser,
+    token, setToken
+  }
+
+  return (
+    <merchant.Provider value={context}>
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <Router>
+        <MainRouter />
+      </Router>
+    </merchant.Provider>
+  );
+}
+
+export default App;
