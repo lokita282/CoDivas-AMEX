d135cb8a9003fda6f17c0b0842efa0247ce61e02
vidhitapai <vidhitapai@gmail.com>
2023-06-17 23:52:09 +0530

    enabled timestamps in logs

--- models/activityLog.js
+++ models/activityLog.js
@@ -1,48 +1,51 @@
 // Importing modules
 const mongoose = require('mongoose');
 
-const activityLogSchema = new mongoose.Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+const activityLogSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        userIPAddress: {
+            type: String
+        },
+        userAgent: {
+            type: String
+        },
+        userType: {
+            type: String,
+            enum: ['merchant', 'bank', 'beneficiary', 'organisation']
+        },
+        uid: {
+            type: String
+        },
+        voucherUid: {
+            type: String
+        },
+        occurredAt: {
+            type: Date
+        },
+        actionType: {
+            type: String,
+            enum: [
+                'Create',
+                'Bulk Create',
+                'Update',
+                'Delete',
+                'Validate',
+                'Redeem',
+                'Revoke',
+                'Log In',
+                'Sign Up'
+            ]
+        },
+        body: {
+            type: String
+        }
     },
-    userIPAddress: {
-        type: String
-    },
-    userAgent: {
-        type: String
-    },
-    userType: {
-        type: String,
-        enum: ['merchant', 'bank', 'beneficiary', 'organisation']
-    },
-    uid: {
-        type: String
-    },
-    voucherUid: {
-        type: String
-    },
-    occurredAt: {
-        type: Date
-    },
-    actionType: {
-        type: String,
-        enum: [
-            'Create',
-            'Bulk Create',
-            'Update',
-            'Delete',
-            'Validate',
-            'Redeem',
-            'Revoke',
-            'Log In',
-            'Sign Up'
-        ]
-    },
-    body: {
-        type: String
-    }
-});
+    { timestamps: true }
+);
 
 const ActivityLog = mongoose.model('ActivityLog', activityLogSchema);
 
