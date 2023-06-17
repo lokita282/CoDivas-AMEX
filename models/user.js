86007a567e2d11e96a69ded17fb807c7eae754cb
vidhitapai <vidhitapai@gmail.com>
2023-06-17 23:51:48 +0530

    added admin role to user schema

--- models/user.js
+++ models/user.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema(
         },
         type: {
             type: String,
-            enum: ['merchant', 'bank', 'beneficiary', 'organisation'],
+            enum: ['merchant', 'bank', 'beneficiary', 'organisation', 'admin'],
             required: true
         },
         merchant: {
