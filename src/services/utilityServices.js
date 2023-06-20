16a7895d17220f9016bc57f3d48ff2b0a4795560
khushi mehta <khushimehta8282@gmail.com>
2023-06-20 20:01:53 +0530

    utility encryp decryp

--- src/services/utilityServices.js
+++ src/services/utilityServices.js
@@ -1,18 +1,25 @@
 import httpcommon from "../httpcommon"
+import { decryptData, encryptData } from "./encryptdecrypt"
 
-export const validateUtility = (data) => {
-    return httpcommon.post(`/beneficiary/utility/validate-voucher`, data, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('codivasToken')}`,
-      },
-    })
-  }
+export const validateUtility = async (data) => {
+  let res = await httpcommon.post(`/beneficiary/utility/validate-voucher`, { data: encryptData(data) }, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('codivasToken')}`,
+    },
+  })
+  let res2 = decryptData(res.data)
+  let res3 = { data: JSON.parse(JSON.parse(res2)) }
+  return res3
+}
 
 
-  export const redeemUtility = (data) => {
-    return httpcommon.post(`/beneficiary/utility/redeem-voucher`, data, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('codivasToken')}`,
-      },
-    })
-  }
+export const redeemUtility = async (data) => {
+  let res = await httpcommon.post(`/beneficiary/utility/redeem-voucher`, { data: encryptData(data) }, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('codivasToken')}`,
+    },
+  })
+  let res2 = decryptData(res.data)
+  let res3 = { data: JSON.parse(JSON.parse(res2)) }
+  return res3
+}
