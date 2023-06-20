79896845b861b3dcb440d59b3862863a94401305
khushi mehta <khushimehta8282@gmail.com>
2023-06-20 16:08:37 +0530

    serviceworker added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+//
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 
+// ----------------------------------------------------------------------
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+root.render(<App />);
+
+// If you want to enable client cache, register instead.
+serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
