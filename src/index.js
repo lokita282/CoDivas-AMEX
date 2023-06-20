241f6fc5ead53d1efe09857a2801cb03f65dd433
khushi mehta <khushimehta8282@gmail.com>
2023-06-20 16:30:12 +0530

    register

--- src/index.js
+++ src/index.js
@@ -12,8 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
 // If you want to enable client cache, register instead.
-serviceWorker.unregister();
-
+serviceWorker.register()
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
